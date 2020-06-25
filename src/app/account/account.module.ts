ae139869645d3b73ff60a08c1c054695ee986224
csaneej <saneej4u@gmail.com>
2020-06-25 18:58:20 +0100

    Add Bootswatch

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -5,6 +5,7 @@ import { RegisterComponent } from './register/register.component';
 import { AccountRoutingModule } from './account-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { AccountComponent } from './account.component';
+import { CoreModule } from '../core/core.module';
 
 
 
@@ -14,7 +15,8 @@ import { AccountComponent } from './account.component';
     CommonModule,
     AccountRoutingModule,
     SharedModule,
-    SharedModule
+    SharedModule,
+    CoreModule
   ]
 })
 export class AccountModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from './account/account.service';
 
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
