260bb2141fa96934b1c53fd023242a09340b930d
Saneej Cherakadavath <c.saneej@hotmail.com>
2020-07-17 21:08:35 +0100

    Code refactoring

--- src/app/shared/components/video-modal/video-modal.component.ts
+++ src/app/shared/components/video-modal/video-modal.component.ts
@@ -22,19 +22,27 @@ export class VideoModalComponent implements OnInit {
       title: 'Pale Blue Dot',
       src: 'http://static.videogular.com/assets/videos/videogular.mp4',
       type: 'video/mp4'
-    },
-    {
-      title: 'Big Buck Bunny',
-      src:
-        'http://static.videogular.com/assets/videos/big_buck_bunny_720p_h264.mov',
-      type: 'video/mp4'
-    },
-    {
-      title: 'Elephants Dream',
-      src: 'http://static.videogular.com/assets/videos/elephants-dream.mp4',
-      type: 'video/mp4'
     }
   ];
+
+  // playlist: Array<IMedia> = [
+  //   {
+  //     title: 'Pale Blue Dot',
+  //     src: 'http://static.videogular.com/assets/videos/videogular.mp4',
+  //     type: 'video/mp4'
+  //   },
+  //   {
+  //     title: 'Big Buck Bunny',
+  //     src:
+  //       'http://static.videogular.com/assets/videos/big_buck_bunny_720p_h264.mov',
+  //     type: 'video/mp4'
+  //   },
+  //   {
+  //     title: 'Elephants Dream',
+  //     src: 'http://static.videogular.com/assets/videos/elephants-dream.mp4',
+  //     type: 'video/mp4'
+  //   }
+  // ];
 
   currentIndex = 0;
   currentItem: IMedia = this.playlist[this.currentIndex];
