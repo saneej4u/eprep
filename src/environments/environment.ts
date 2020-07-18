a347bbf89cc708de2cbf16baea83eafff7d867f1
csaneej <saneej4u@gmail.com>
2020-07-18 08:53:36 +0100

    Add dev env

---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: false,
+    firebaseConfig: {
+      apiKey: "AIzaSyBg869xkaqW3B1nZixQWZjdM77KKip6Bcw",
+      authDomain: "novus-75133.firebaseapp.com",
+      databaseURL: "https://novus-75133.firebaseio.com",
+      projectId: "novus-75133",
+      storageBucket: "novus-75133.appspot.com",
+      messagingSenderId: "593006177882",
+      appId: "1:593006177882:web:246f117d951a8549a0f263",
+      measurementId: "G-27PN01RVCG"
+    }
+  };
+  
+  /*
+   * For easier debugging in development mode, you can import the following file
+   * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+   *
+   * This import should be commented out in production mode because it will have a negative impact
+   * on performance if an error is thrown.
+   */
+  // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+  
